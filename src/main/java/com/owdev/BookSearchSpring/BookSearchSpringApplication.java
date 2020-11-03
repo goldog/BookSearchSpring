3c521ffbec81ec6f5ca4c15bf3b345e9cca05319
goldog <goldog83@gmail.com>
2020-11-03 13:30:35 +0900

    프로젝트 최초 실행

---
+++ src/main/java/com/owdev/BookSearchSpring/BookSearchSpringApplication.java
@@ -0,0 +1,13 @@
+package com.owdev.BookSearchSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSearchSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owdev/BookSearchSpring/BookSearchSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owdev.BookSearchSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
