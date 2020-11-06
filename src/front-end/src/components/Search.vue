cdd65c41365e734ec052893ceb2c73b39e04c501
goldog <goldog83@gmail.com>
2020-11-06 17:48:34 +0900

    Vue 프로젝트 파일 추가

---
+++ src/front-end/src/components/Search.vue
@@ -0,0 +1,70 @@
+<template>
+  
+  <div class="input search keyword">
+    <h1>{{ msg }}</h1>
+    책 정보: 
+    
+    <input type="text" v-model="keyword" placeholder="검색할 책의 제목 혹은 ISBN을 입력하세요" v-on:keypress.enter="searchKeyword" >
+    
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">제목 혹은 ISBN을 입력하세요.
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
+    <modal v-if="resModal" @close="resModal = false">
+      <h3 slot="header">결과</h3>
+      <span slot="footer" @click="resModal = false"> {{keyValue}}
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
+
+  </div>
+  
+</template>
+
+<script>
+
+import Modal from './common/Modal.vue'
+
+export default {
+  data() {
+    return { 
+      keyword: '',
+      keyValue: '',
+      resModal: false,
+      showModal: false
+    }
+  },
+  methods: {
+      searchKeyword() {
+        if (this.keyword !== "") { 
+            var value = this.keyword && this.keyword.trim();
+            this.keyValue = value;
+            this.resModal = !this.resModal;
+            this.clearInput();
+        } else if (this.resModal == false) {
+            this.showModal = !this.showModal;
+        } else {
+            this.resModal = !this.resModal;
+        }
+      },
+      clearInput() {
+          this.keyword = ''
+      } 
+  },
+  components: {
+    Modal: Modal
+  },
+  name: 'Search',
+  props: {
+    msg: String
+  }
+  
+
+}
+</script>
+
+<style>
+
+</style>
